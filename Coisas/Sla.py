487370a4a61a743f97dad2f65096e46c4bc95907
C4rl-LP <al.carlos.pereira@impatech.edu.br>
2026-01-12 12:02:44 -0300

    Update Sla.py

--- Coisas/Sla.py
+++ Coisas/Sla.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 N = 1000000
 n = 0
@@ -8,3 +9,13 @@ for _ in range(N):
     if (x-1)**2 + (y-1)**2 < 1:
         n +=1
 print(n/N*4) 
+
+x = np.random.multivariate_normal([0,0], [[1,0],
+                                        [0,1]], 100)
+
+norms = np.linalg.norm(x, axis=1, keepdims=True)
+print(norms)
+x = x/norms
+
+plt.scatter(x[:,0], x[:,1])
+plt.show()
